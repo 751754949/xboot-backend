9bd4318ec047fde48030ffa739a3c09269f21196
yangm0910 <yangm_1111@163.com>
2019-04-18 17:06:37 +0800

    update  onMessage,only send message to specialized person.

--- src/main/java/cn/exrick/xboot/modules/chatOnline/WebSocketServer.java
+++ src/main/java/cn/exrick/xboot/modules/chatOnline/WebSocketServer.java
@@ -1,6 +1,6 @@
 package cn.exrick.xboot.modules.chatOnline;
 
-import com.alibaba.druid.util.StringUtils;
+import cn.hutool.json.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -20,7 +20,7 @@ public class WebSocketServer {
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
     private static int onlineCount = 0;
     //创建一个线程安全的map
-    private static Map<String,WebSocketServer> users = Collections.synchronizedMap(new HashMap());
+    private static Map<String, WebSocketServer> users = Collections.synchronizedMap(new HashMap());
 
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
@@ -33,12 +33,11 @@ public class WebSocketServer {
      */
     @OnOpen
     public void onOpen(Session session, @PathParam("userId") String username) {
-        this.session=session;
-        this.username =username;
-//        sessions.add(session);
-//        if(sessions.size()>1)log.info(sessions.get(0).equals(sessions.get(1))+"");
+        this.session = session;
+        this.username = username;
+        sessions.add(session);
         addOnlineCount(username);           //在线数加1
-        log.info(username+"加入！当前在线人数为" + getOnlineCount());
+        log.info(username + "加入！当前在线人数为" + getOnlineCount());
         try {
             this.session.getBasicRemote().sendText("连接成功");
         } catch (IOException e) {
@@ -58,59 +57,41 @@ public class WebSocketServer {
 
     /**
      * 收到客户端消息后触发的方法
+     *
      * @param message 客户端发送过来的消息
      */
     @OnMessage
-    public void onMessage(String message) {
+    public void onMessage(String message) throws IOException {
         log.info("来自客户端的消息:" + message);
-        //群发消息
-        try {
-            if (StringUtils.isEmpty(message)){
-                return ;
-            }
-            //如果给所有人发消息携带@ALL, 给特定人发消息携带@xxx@xxx#message
-            String[] split = message.split("#");
-            if (split.length>1){
-                String[] users = split[0].split("@");
-                if (users.length<2){return;}
-                String firstuser = users[1].trim();
-                if (StringUtils.isEmpty(firstuser)||"ALL".equals(firstuser.toUpperCase())){
-                    String msg =username +": "+ split[1];
-                    sendInfo(msg);//群发消息
-                }else{//给特定人员发消息
-                    for (String user : users) {
-                        if (!StringUtils.isEmpty(user.trim())){
-                            sendMessageToSomeBody(user.trim(),split[1]);
-                        }
-                    }
-                }
-            }else{
-                sendInfo(username +": "+message);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+//        new JSONObject(message).toString()
+        JSONObject jsonObject = new JSONObject(message);
+        String receiver = (String) jsonObject.get("receiver");
+        String content = (String) jsonObject.get("content");
+
+        if (receiver != null) {
+            sendMessageToSomeBody(receiver, content);
         }
     }
 
     @OnError
     public void onError(Session session, Throwable error) {
-        log.error("发生错误 session: "+session);
+        log.error("发生错误 session: " + session);
         error.printStackTrace();
     }
 
     //    给特定人员发送消息
-    public void sendMessageToSomeBody(String username,String message) throws IOException {
-        if(users.get(username)==null){
+    public void sendMessageToSomeBody(String username, String message) throws IOException {
+        if (users.get(username) == null) {
             return;
         }
         users.get(username).session.getBasicRemote().sendText(message);
-        this.session.getBasicRemote().sendText(this.username+"@"+username+": "+message);
+        this.session.getBasicRemote().sendText(message);
     }
 
     /**
      * 群发自定义消息
      */
-    public  void sendInfo(String message) throws IOException {
+    public void sendInfo(String message) throws IOException {
         for (WebSocketServer item : users.values()) {
             try {
                 item.session.getBasicRemote().sendText(message);
@@ -124,16 +105,16 @@ public class WebSocketServer {
         return onlineCount;
     }
 
-    public  synchronized void addOnlineCount(String username) {
-        int beforesize = this.users.size();
+    public synchronized void addOnlineCount(String username) {
+        int before = this.users.size();
         users.put(username, this);   //加入map中,为了测试方便使用username做key
         int after = users.size();
-        if (after > beforesize){
+        if (after > before) {
             WebSocketServer.onlineCount++;
         }
     }
 
     public static synchronized void subOnlineCount() {
-        WebSocketServer.onlineCount--;
+        if (WebSocketServer.onlineCount > 0) WebSocketServer.onlineCount--;
     }
 }
